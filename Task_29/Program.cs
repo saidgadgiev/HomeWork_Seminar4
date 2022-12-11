5a52ede136b9bd06a4f20fe489d2627325f877af
saidgadgiev <117773666+saidgadgiev@users.noreply.github.com>
2022-12-12 00:55:11 +0300

    Finish HomeWork

--- Task_29/Program.cs
+++ Task_29/Program.cs
@@ -6,3 +6,14 @@
 6, 1, 33 -> [6, 1, 33]
 */
 
+Console.WriteLine("Будет создан массив из 8 элементов.");
+
+int[] number = new int[8];
+Random rnd = new Random();
+Console.Write("[");
+for (int i = 0; i < number.Length; i++)
+{
+    number[i]=rnd.Next(0,100);
+    Console.Write(number[i] + (i < number.Length-1 ?", ":""));
+}
+Console.Write("]");
